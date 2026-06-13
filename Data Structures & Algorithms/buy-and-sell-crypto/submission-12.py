d1a8075a14bcc62f0bd5380c24ec7a75f368aab7
MO <officiallymoaizattiq@gmail.com>
2026-06-13 12:09:10 +0500

    Add: buy-and-sell-crypto - submission-12

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-12.py
@@ -0,0 +1,15 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        profit=0
+        l=0
+        mimimum=prices[l]
+        for r in range(1,len(prices)):
+            currlow=prices[l]
+            l += 1 
+            mimimum=min(mimimum,currlow)
+            curr=prices[r]-mimimum
+            profit=max(profit,curr)
+        return profit
+            
+
+        
